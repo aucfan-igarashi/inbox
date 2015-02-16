1996d809aa0bdc1f1f65504b1b8740e6754907ab
vagrant <vagrant@vagrant.box>
2015-02-16 23:17:39 +0900

    update 20150216

---
+++ ruby/for.rb
@@ -0,0 +1,17 @@
+### num = 0
+### while num < 2 do
+###   print("num = ", num)
+###   num = num + 1
+### end
+### puts("End")
+
+
+num = 0
+puts("繰り返し開始\n")
+
+while num < 5 do
+  print("num =", num, "\n")
+  num = num + 1
+end
+
+puts("繰り返し完了\n")
